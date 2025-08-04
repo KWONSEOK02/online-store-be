35fa76a7ca5ba1e06236919d14d098a196ee2a5d
gs07103 <gwonseok02@email.com>
2025-08-04 18:20:57 +0900

    토큰 로그인, 상품 생성 구현

--- routes/user.api.js
+++ routes/user.api.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
-
+const authController = require("..//controllers//auth.controller");
 
 
 // 1. 회원가입 endpoint
 router.post("/", userController.createUser);
+
+router.get("/me", authController.authenticate, authController.getUser); // 프론트에서 header로 보내서 get 사용
 
 module.exports = router;
