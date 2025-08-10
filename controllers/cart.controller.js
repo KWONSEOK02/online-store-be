fd4111afa9aa813f31ec00fc2cfb8450468512a5
gs07103 <gwonseok02@email.com>
2025-08-11 01:14:27 +0900

    cart 삭제, 수정, 수량 변경 구현

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -53,4 +53,56 @@ cartController.getCart = async (req, res) => {
   }
 };
 
+
+cartController.deleteCartItem = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req;
+
+    const cart = await Cart.findOne({ userId });
+
+    cart.items = cart.items.filter((item) => !item._id.equals(id));
+
+    await cart.save();
+    res.status(200).json({ status: "success", cartItemQty: cart.items.length });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+cartController.editCartItem = async (req, res) => {
+  try {
+    const { userId } = req;
+    const { id } = req.params; // 상품 id
+
+    const { qty } = req.body;
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "items",
+      populate: {
+        path: "productId",
+        model: "Product",
+      },
+    });
+    if (!cart) throw new Error("There is no cartItem for this user");
+    const index = cart.items.findIndex((item) => item._id.equals(id)); // 상품들이 인덱스 형식으로 저장되어 있음
+    if (index === -1) throw new Error("Can not find item"); //-1 가 존재 가능? 
+    cart.items[index].qty = qty;
+    await cart.save();
+    res.status(200).json({ status: "success", data: cart.items });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+cartController.getCartQty = async (req, res) => {
+  try {
+    const { userId } = req;
+    const cart = await Cart.findOne({ userId: userId });
+    if (!cart) throw new Error("There is no cartItem!");
+    res.status(200).json({ status: "success", qty: cart.items.length });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
 module.exports = cartController;
--- routes/cart.api.js
+++ routes/cart.api.js
@@ -3,12 +3,24 @@ const router = express.Router();
 const authController = require("../controllers/auth.controller");
 const cartController = require("../controllers/cart.controller");
 
+//카트는 로그인을 해야 수정할 수 있어서 인증 필요
 router.post("/",
     authController.authenticate,
      cartController.addItemToCart);
 
-     router.get("/",
-        authController.authenticate,
-         cartController.getCart);
+router.get("/",
+    authController.authenticate,
+    cartController.getCart);
+
+router.delete( // 삭제할 상품 카트에서의 id 제공
+    "/:id",
+    authController.authenticate,
+    cartController.deleteCartItem);
+      
+router.put("/:id", authController.authenticate, cartController.editCartItem);
+// 수정할 상품 카트에서의  id 제공
+
+router.get("/qty", authController.authenticate, cartController.getCartQty);
+
 
 module.exports = router;
